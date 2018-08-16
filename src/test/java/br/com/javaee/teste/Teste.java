7c382c0a2aa84557486b39a4733cde71a0ddea90
pereirls <lucas_sp100@hotmail.com>
2018-08-15 23:08:26 -0300

    15/08/2018 23:08

--- src/test/java/br/com/javaee/teste/Teste.java
+++ src/test/java/br/com/javaee/teste/Teste.java
@@ -1,10 +1,15 @@
 package br.com.javaee.teste;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.inject.Inject;
 
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.junit.InSequence;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.wildfly.common.Assert;
@@ -20,15 +25,14 @@ public class Teste {
 	
 	private Conta conta;
 	
-    /*@Deployment
+    @Deployment
     public static JavaArchive arquivoTeste() {
         return ShrinkWrap.create(JavaArchive.class)
                 .addClasses(ContaBean.class,
-                        
                         Conta.class)
                 .addAsResource("META-INF/persistence.xml")
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-    }*/
+    }
 	
 
 	public Teste() {
